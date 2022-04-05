e17f3ab8c03df9d8dd61647762d69f07c6e666ae
The-J0Y <102557305+The-J0Y@users.noreply.github.com>
2022-04-04 18:04:43 -0700

    Update player.ex

--- lib/monitor/player.ex
+++ lib/monitor/player.ex
@@ -1,10 +1,4 @@
 defmodule Monitor.Player do
-  @moduledoc """
-  Module Monitor.Player monitors some respective summoner that the GenServer is
-  initially specified to for new matches & utilizes OTP to schedule this task
-  every single minute for the next hour. If this summoner manages to complete a
-  match within this interval, the match id is thus logged onto the console.
-  """
   use GenServer
   require Logger
 
@@ -14,32 +8,10 @@ defmodule Monitor.Player do
 
   ##### clientside api ########################################################
 
-  @doc """
-  start_link/1 starts the Player GenServer by calling GenServer.start_link/3
-  with the current module as the first parameter & a list of its respective
-  summoner's stats.
-
-  parameters___________________________________________________________________
-  name
-  summoner name string
-
-  prv
-  platform routing value string
-
-  puuid
-  player universally unique identifier string
-  """
   def start_link([name, prv, puuid]) do
     GenServer.start_link(__MODULE__, [name, prv, puuid])
   end
 
-  @doc """
-  child_spec/1 builds the specification of this child server to be supervised,
-  specifying that the puuid of the summoner serves as the unique identifier per
-  Player server.
-
-  Function parameter is that of the same as start_link/1
-  """
   def child_spec([name, prv, puuid]) do
     %{id: puuid, start: {__MODULE__, :start_link, [[name, prv, puuid]]}}
   end
@@ -47,17 +19,7 @@ defmodule Monitor.Player do
 
   ##### server callbacks ######################################################
 
-  @doc """
-  init/1 schedules two asynchronous tasks then takes the child specifications
-  of the summoner to initialize the overall server state as a hashmap with all
-  the parameter list entries in addition to another key utilized for keeping
-  track of each summoner's most recent match.
-  """
   def init([name, prv, puuid]) do
-
-    # Initial asynchronous calls to the module are made to start both tasks of
-    # monitoring new matches every minute & shutting down the server after an
-    # hour, respectively.
     schedule_sweep(:minute_interval)
     schedule_sweep(:hour_window)
 
@@ -71,32 +33,12 @@ defmodule Monitor.Player do
     {:ok, summoner}
   end
 
-  @doc """
-  handle_info/2 function when pattern-matched to the :minute_interval message
-  by the module makes a call to helper function new_match?/1 to determine
-  whether the summoner has completed a new match.
-
-  parameters___________________________________________________________________
-  :minute_interval
-  atom message specifying to the module how to handle this particular
-  asynchronous call pattern-matched said message
-
-  summoner
-  hashmap server state containing keys specifying the match id of the most
-  recent match, summoner name, platform routing value (prv), & puuid
-  """
   def handle_info(:minute_interval, summoner) do
     case new_match?(summoner) do
 
-      # If no new match, pass server state unchanged
       nil ->
         {:noreply, summoner}
-
-      # If a new match has been played, log said match id onto console &
-      # schedule another asynchronous call to be made the following minute &
-      # update the server state.
-      # Note that since the initial state to last_mid is the empty string, once
-      # a call is made to update it, it does not need to be logged.
+        
       latest_match ->
         if summoner.last_mid == "" do
           {:noreply, %{summoner | last_mid: latest_match}}
@@ -108,15 +50,6 @@ defmodule Monitor.Player do
     end
   end
 
-  # """
-  # handle_info/2 function when pattern-matched to the :hour_window message by
-  # the module shuts down the entire monitoring process after an hour
-  #
-  # parameters_________________________________________________________________
-  # :hour_window
-  # atom message specifying to the module how to handle this particular
-  # asynchronous call pattern-matched said message
-  # """
   def handle_info(:hour_window, _summoner) do
     exit :shutdown
   end
@@ -124,19 +57,14 @@ defmodule Monitor.Player do
 
   ##### helper functions ######################################################
 
-  # Delayed call to module after a minute with message :minute_interval
   defp schedule_sweep(:minute_interval) do
     Process.send_after(self(), :minute_interval, @minute)
   end
-
-  # Delayed call to module after an hour with message :hour_window
+  
   defp schedule_sweep(:hour_window) do
     Process.send_after(self(), :hour_window, @hour)
   end
 
-  # Helper function new_match?/1 makes a GET request to the developer API to
-  # get the match id & loop another asynchronous call the following minute.
-  # Returns nil no new match, retursn the latest match id otherwise
   defp new_match?(summoner) do
     latest_match = hd(Monitor.Limiter.json_of(:for_match_id, summoner))
 
